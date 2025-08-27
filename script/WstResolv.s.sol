28fd6146d9bc34f192a8d9bcacb74ee5b4fd7b8b
BitWonka <79578887+BitWonka@users.noreply.github.com>
2025-08-27 05:41:18 +0200

    fmt

--- script/WstResolv.s.sol
+++ script/WstResolv.s.sol
@@ -5,18 +5,18 @@ import "../src/WstResolv.sol";
 import {Script, console} from "forge-std/Script.sol";
 
 contract DeployWstResolv is Script {
-	address public STRESOLV = 0xFE4BCE4b3949c35fB17691D8b03c3caDBE2E5E23;
-	address public RESOLV = 0x259338656198eC7A76c729514D3CB45Dfbf768A1;
+    address public STRESOLV = 0xFE4BCE4b3949c35fB17691D8b03c3caDBE2E5E23;
+    address public RESOLV = 0x259338656198eC7A76c729514D3CB45Dfbf768A1;
 
-	function run() external {
-		string memory NAME = "Wrapped Staked RESOLV";
-		string memory SYMBOL = "wstRESOLV";
-		address OWNER = vm.envAddress("OWNER");
+    function run() external {
+        string memory NAME = "Wrapped Staked RESOLV";
+        string memory SYMBOL = "wstRESOLV";
+        address OWNER = vm.envAddress("OWNER");
 
-		vm.startBroadcast();
+        vm.startBroadcast();
 
-		WstResolv vault = new WstResolv(STRESOLV, RESOLV, NAME, SYMBOL, OWNER);
+        WstResolv vault = new WstResolv(STRESOLV, RESOLV, NAME, SYMBOL, OWNER);
 
-		vm.stopBroadcast();
-	}
+        vm.stopBroadcast();
+    }
 }
--- src/WstResolv.sol
+++ src/WstResolv.sol
@@ -20,133 +20,133 @@ import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 interface IStResolv {
-	function claim(address _user, address _receiver) external;
+    function claim(address _user, address _receiver) external;
 
-	function deposit(uint256 _amount, address _receiver) external;
+    function deposit(uint256 _amount, address _receiver) external;
 }
 
 contract WstResolv is ERC4626, Ownable, Pausable, ReentrancyGuard {
-	using SafeERC20 for IERC20;
+    using SafeERC20 for IERC20;
 
-	IStResolv public immutable stResolv; // stRESOLV proxy (also staking contract)
-	IERC20 public immutable resolv; // reward token received on claim()
+    IStResolv public immutable stResolv; // stRESOLV proxy (also staking contract)
+    IERC20 public immutable resolv; // reward token received on claim()
 
-	bool public harvestOnDeposit = false;
-	uint256 public minHarvestInterval = 1 hours;
-	uint256 public lastHarvest;
+    bool public harvestOnDeposit = false;
+    uint256 public minHarvestInterval = 1 hours;
+    uint256 public lastHarvest;
 
-	event Harvest(uint256 gained);
-	event HarvestConfigSet(bool onDeposit, uint256 minInterval);
+    event Harvest(uint256 gained);
+    event HarvestConfigSet(bool onDeposit, uint256 minInterval);
 
-	constructor(
-		address _stResolv, // underlying
-		address _resolv, // reward token
-		string memory _name,
-		string memory _symbol,
-		address _owner
-	) ERC20(_name, _symbol) ERC4626(IERC20(_stResolv)) Ownable(_owner) {
-		stResolv = IStResolv(_stResolv);
-		resolv = IERC20(_resolv);
-		// we deposit resolv for stresolv immediately, so we safely set max approve
-		SafeERC20.forceApprove(resolv, _stResolv, type(uint256).max);
-	}
+    constructor(
+        address _stResolv, // underlying
+        address _resolv, // reward token
+        string memory _name,
+        string memory _symbol,
+        address _owner
+    ) ERC20(_name, _symbol) ERC4626(IERC20(_stResolv)) Ownable(_owner) {
+        stResolv = IStResolv(_stResolv);
+        resolv = IERC20(_resolv);
+        // we deposit resolv for stresolv immediately, so we safely set max approve
+        SafeERC20.forceApprove(resolv, _stResolv, type(uint256).max);
+    }
 
-	/* Owner functions */
-	function pause() external onlyOwner {
-		_pause();
-	}
+    /* Owner functions */
+    function pause() external onlyOwner {
+        _pause();
+    }
 
-	function unpause() external onlyOwner {
-		_unpause();
-	}
+    function unpause() external onlyOwner {
+        _unpause();
+    }
 
-	function setHarvestConfig(bool onDeposit, uint256 minInterval) external onlyOwner {
-		harvestOnDeposit = onDeposit;
-		minHarvestInterval = minInterval;
-		emit HarvestConfigSet(onDeposit, minInterval);
-	}
+    function setHarvestConfig(bool onDeposit, uint256 minInterval) external onlyOwner {
+        harvestOnDeposit = onDeposit;
+        minHarvestInterval = minInterval;
+        emit HarvestConfigSet(onDeposit, minInterval);
+    }
 
-	function rescueERC20(address token, address to, uint256 amount) external onlyOwner {
-		require(token != address(asset()), "cannot rescue asset");
-		require(token != address(this), "cannot rescue shares");
-		IERC20(token).safeTransfer(to, amount);
-	}
+    function rescueERC20(address token, address to, uint256 amount) external onlyOwner {
+        require(token != address(asset()), "cannot rescue asset");
+        require(token != address(this), "cannot rescue shares");
+        IERC20(token).safeTransfer(to, amount);
+    }
 
-	/* harvest logic */
+    /* harvest logic */
 
-	function harvest() external nonReentrant returns (uint256 gained) {
-		return _executeHarvest();
-	}
+    function harvest() external nonReentrant returns (uint256 gained) {
+        return _executeHarvest();
+    }
 
-	function _harvest() internal {
-		if (!harvestOnDeposit) return;
-		_executeHarvest();
-	}
+    function _harvest() internal {
+        if (!harvestOnDeposit) return;
+        _executeHarvest();
+    }
 
-	function _executeHarvest() internal returns (uint256 gained) {
-		if (block.timestamp < lastHarvest + minHarvestInterval) return 0;
+    function _executeHarvest() internal returns (uint256 gained) {
+        if (block.timestamp < lastHarvest + minHarvestInterval) return 0;
 
-		address vault = address(this);
-		uint256 beforeBal = IERC20(asset()).balanceOf(address(this));
+        address vault = address(this);
+        uint256 beforeBal = IERC20(asset()).balanceOf(address(this));
 
-		// 1) claim resolv
-		stResolv.claim(vault, vault);
+        // 1) claim resolv
+        stResolv.claim(vault, vault);
 
-		// 2) stake all RESOLV into stRESOLV
-		uint256 resolvBalance = resolv.balanceOf(vault);
-		if (resolvBalance != 0) {
-			stResolv.deposit(resolvBalance, vault); // mints stRESOLV to this vault
-		}
+        // 2) stake all RESOLV into stRESOLV
+        uint256 resolvBalance = resolv.balanceOf(vault);
+        if (resolvBalance != 0) {
+            stResolv.deposit(resolvBalance, vault); // mints stRESOLV to this vault
+        }
 
-		uint256 afterBal = IERC20(asset()).balanceOf(vault);
-		gained = afterBal - beforeBal;
-		lastHarvest = block.timestamp;
-		if (gained != 0) emit Harvest(gained);
-	}
+        uint256 afterBal = IERC20(asset()).balanceOf(vault);
+        gained = afterBal - beforeBal;
+        lastHarvest = block.timestamp;
+        if (gained != 0) emit Harvest(gained);
+    }
 
-	/* overrides */
+    /* overrides */
 
-	function deposit(uint256 assets, address receiver)
-	public
-	override
-	whenNotPaused
-	nonReentrant
-	returns (uint256 shares)
-	{
-		_harvest();
-		shares = super.deposit(assets, receiver);
-	}
+    function deposit(uint256 assets, address receiver)
+        public
+        override
+        whenNotPaused
+        nonReentrant
+        returns (uint256 shares)
+    {
+        _harvest();
+        shares = super.deposit(assets, receiver);
+    }
 
-	function mint(uint256 shares, address receiver)
-	public
-	override
-	whenNotPaused
-	nonReentrant
-	returns (uint256 assets)
-	{
-		_harvest();
-		assets = super.mint(shares, receiver);
-	}
+    function mint(uint256 shares, address receiver)
+        public
+        override
+        whenNotPaused
+        nonReentrant
+        returns (uint256 assets)
+    {
+        _harvest();
+        assets = super.mint(shares, receiver);
+    }
 
-	function withdraw(uint256 assets, address receiver, address owner_)
-	public
-	override
-	whenNotPaused
-	nonReentrant
-	returns (uint256 shares)
-	{
-		// No harvest here.
-		shares = super.withdraw(assets, receiver, owner_);
-	}
+    function withdraw(uint256 assets, address receiver, address owner_)
+        public
+        override
+        whenNotPaused
+        nonReentrant
+        returns (uint256 shares)
+    {
+        // No harvest here.
+        shares = super.withdraw(assets, receiver, owner_);
+    }
 
-	function redeem(uint256 shares, address receiver, address owner_)
-	public
-	override
-	whenNotPaused
-	nonReentrant
-	returns (uint256 assets)
-	{
-		// No harvest here.
-		assets = super.redeem(shares, receiver, owner_);
-	}
+    function redeem(uint256 shares, address receiver, address owner_)
+        public
+        override
+        whenNotPaused
+        nonReentrant
+        returns (uint256 assets)
+    {
+        // No harvest here.
+        assets = super.redeem(shares, receiver, owner_);
+    }
 }
